041993f6b42a38ac586f17c3c18937ce7218f36a
等风来 <3139373986@qq.com>
2024-04-30 18:44:00 +0800

    大图支持参数

--- apps/BigImg.js
+++ apps/BigImg.js
@@ -11,19 +11,45 @@ export class bigPicture extends plugin {
       priority: 5000,
       rule: [
         {
-          reg: `^#?(转|发)?大图$`,
-          fnc: "ImageLink",
+          reg: '^#?转?大图', // 转大图
+          fnc: "imageCard"
         },
       ]
     });
   }
 
+  async imageCard(e) {
+    /** 获取图片链接 */
+    const img = await this.ImageLink(e)
+    if (!img) return false
+    
+    /** 处理参数 */
+    const token = e.msg.replace(/^#?转?大图/, "").trim()
+    const parts = token.split(':')
+    /** 标题 */
+    const title = parts[0] ||''
+    /** 子标题 */
+    const sub = parts[1] || ''
+    /** 外显 */
+    const yx = parts[2] || ''
+    
+    let link, msg;
+    for (let i of img) {
+      /** 双重编码URL */
+      const encoded = encodeURIComponent(i);
+      link = encodeURIComponent(encoded);
+      /** 生成卡片代码 */
+      msg = await this.DT(link, title, sub, yx);
+      await e.reply(segment.json(msg));
+    }
+  }
+
   /**
-   * 获取图片链接
-   * @param e
+   * 工具函数：获取图片链接
+   * @param e - 消息事件
+   * @return img - 图片链接数组
    */
   async ImageLink(e) {
-    // 获取图片链接
     let img = [];
     if (e.source) {
       let source;
@@ -39,31 +65,23 @@ export class bigPicture extends plugin {
 
     if (_.isEmpty(img)) {
       await this.reply("⚠ 请回复或带图使用");
-      return;
+      return false
     }
     await e.reply(`✅ 检测到${img.length}张图片`);
-
-    // 获取卡片
-    let a, msg;
-    for (let i of img) {
-        // 双重编码
-        const encoded = encodeURIComponent(i);
-        a = encodeURIComponent(encoded)
-//      a = await this.e.bot.uploadImage(`${i}`);
-      msg = await this.DT(a, '喵喵喵！');
-      e.reply(segment.json(msg));
-    }
+    return img
   }
 
   /** 
-   * 工具函数：卡片签名
+   * 工具函数：生成卡片代码
    * @param link - 图片地址
+   * @param title - 标题
+   * @param subtitle - 子标题
    * @param yx - 卡片外显
-   * @return data - 返回数据
+   * @return data - json卡片代码
    */
-  async DT(link, yx = '[图转卡]') {
-    logger.mark("卡片签名：", link)
-    const response = await fetch(`http://api.mrgnb.cn/API/qq_ark37.php?url=${link}&yx=${yx}`);
+  async DT(link, title='', subtitle='', yx='[图转卡]') {
+    logger.mark("卡片签名:", link)
+    const response = await fetch(`http://api.mrgnb.cn/API/qq_ark37.php?url=${link}&title=${title}&subtitle=${subtitle}&yx=${yx}`);
     const data = await response.text();
     return data;
   }
