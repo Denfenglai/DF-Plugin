98fbb8a53fc0580f3a64b652c7f773f3d71a1984
等风来 <3139373986@qq.com>
2024-04-30 12:42:33 +0800

    新增转大图功能
    使用第三方api,如有问题不担责

---
+++ apps/转大图.js
@@ -0,0 +1,67 @@
+import fetch from "node-fetch";
+import _ from "lodash";
+
+export class bigPicture extends plugin {
+
+  constructor() {
+    super({
+      name: "转大图",
+      dsc: "发送卡片大图",
+      event: "message",
+      priority: 5000,
+      rule: [
+        {
+          reg: `^#?(转|发)?大图$`,
+          fnc: "ImageLink",
+        },
+      ]
+    });
+  }
+
+  /**
+   * 获取图片链接
+   * @param e
+   */
+  async ImageLink(e) {
+    // 获取图片链接
+    let img = [];
+    if (e.source) {
+      let source;
+      if (e.isGroup) {
+        source = (await e.group.getChatHistory(e.source.seq, 1)).pop();
+      } else {
+        source = (await e.friend.getChatHistory(e.source.time, 1)).pop();
+      }
+      img = source.message.filter(i => i.type === "image").map(i => i.url);
+    } else {
+      img = e.img;
+    }
+
+    if (_.isEmpty(img)) {
+      this.setContext("_ImageLinkContext");
+      await this.reply("⚠ 请发送图片");
+      return;
+    }
+    await e.reply(`✅ 检测到${img.length}张图片`);
+    
+    // 获取卡片
+    let a, msg;
+    for (let i of img) {
+      a = await this.e.bot.uploadImage(`${i}`);
+      msg = await this.DT(a['url']);
+      e.reply(segment.json(msg));
+    }
+  }
+
+  /** 
+   * 工具函数：卡片签名
+   * @param link - 图片地址列表
+   * @return msg - 消息列表
+   */
+  async DT(link) {
+    logger.mark("卡片签名：", link)
+    const response = await fetch(`http://api.mrgnb.cn/API/qq_ark37.php?url=${link}`);
+    const data = await response.text();
+    return data;
+  }
+}
