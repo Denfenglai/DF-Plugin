8a0b2d70550cbe4071ce491800a4e89c41e2e3e3
等风来 <3139373986@qq.com>
2024-05-27 20:13:50 +0800

    ⚡ 细节优化
    Signed-off-by: 等风来 <3139373986@qq.com>

--- apps/sendMaster.js
+++ apps/sendMaster.js
@@ -3,6 +3,7 @@ import common from "../../../lib/common/common.js"
 import { Config } from "../components/index.js"
 
 const key = "DF:contact"
+let isSend = false
 
 export class Example extends plugin {
   constructor() {
@@ -21,10 +22,11 @@ export class Example extends plugin {
   }
 
   async contact(e) {
+    if (isSend) return e.reply("❎ 已有发送任务正在进行中，请稍候重试")
     let { open, cd, BotId } = Config.sendMaster
-    if (!open) return e.reply("该功能暂未开启，请先让主人开启才能用哦")
+    if (!open) return e.reply("❎ 该功能暂未开启，请先让主人开启才能用哦")
 
-    if (await redis.get(key)) return e.reply("操作频繁，请稍后再试！")
+    if (await redis.get(key)) return e.reply("❎ 操作频繁，请稍后再试！")
 
     if (e.message.length > 0 && e.message[0].text) {
       e.message[0].text = e.message[0].text.replace(/#?联系主人/, "")
@@ -59,6 +61,8 @@ export class Example extends plugin {
     msg.push(...e.message)
 
     if (BotId == 0) { BotId = bot }
+
+    isSend = true
     await this.sendMasterMsg(msg, BotId)
       .then(() => e.reply(`✅ 消息已送达\n主人的QQ：${Config.masterQQ[0]}`))
       .then(() => redis.set(key, "1", { EX: cd }))
@@ -66,6 +70,7 @@ export class Example extends plugin {
         e.reply(`❎ 消息发送失败，请尝试自行联系：${Config.masterQQ[0]}\n错误信息：${err}`)
         logger.error(err)
       })
+    isSend = false
   }
 
   /**
@@ -80,12 +85,12 @@ export class Example extends plugin {
     if (master?.length) masterQQ = master
     if (Master == 1) {
       if (Bot?.sendMasterMsg) {
-        await Bot.sendMasterMsg(msg)
+        await Bot.sendMasterMsg(msg, Bot.uin, 2000)
       } else {
         for (const i of masterQQ) {
           await common.relpyPrivate(i, msg, botUin)
         }
-        await common.sleep(5000)
+        await common.sleep(2000)
       }
     } else if (Master == 0) {
       await common.relpyPrivate(masterQQ[0], msg, botUin)
