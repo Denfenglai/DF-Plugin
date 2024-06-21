1c02434ae30fbd2ccca298d3434f3fc9ce974cd7
等风来 <3139373986@qq.com>
2024-06-21 16:46:21 +0800

    🚸 支持锅巴
    Signed-off-by: 等风来 <3139373986@qq.com>

---
+++ guoba.support.js
@@ -0,0 +1,138 @@
+import { Config } from "./components/index.js"
+
+/**
+ *  支持锅巴
+ *  锅巴插件：https://gitee.com/guoba-yunzai/guoba-plugin.git
+ *  组件类型，可参考 https://antdv.com/components/overview-cn/
+ */
+
+export function supportGuoba() {
+  return {
+    pluginInfo: {
+      name: "DF-Plugin",
+      title: "DF-Plugin",
+      description: "提供Yunzai-Bot拓展功能",
+      author: [
+        "@等风来",
+        "@DF"
+      ],
+      authorLink: [
+        "https://gitee.com/DengFengLai-F",
+        "https://gitee.com/DenFengLai"
+      ],
+      // 仓库地址
+      link: "https://gitee.com/DenFengLai/DF-Plugin",
+      isV3: true,
+      isV2: false,
+      showInMenu: "auto",
+      // 显示图标，此为个性化配置
+      // 图标可在 https://icon-sets.iconify.design 这里进行搜索
+      icon: "svg-spinners:blocks-wave",
+      // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
+      iconColor: "#d19f56"
+      // 如果想要显示成图片，也可以填写图标路径（绝对路径）
+      // iconPath: path.join(_paths.pluginRoot, 'resources/images/icon.png'),
+    },
+    configInfo: {
+      // 配置项 schemas
+      schemas: [
+        {
+          component: "Divider",
+          label: "联系主人设置"
+        },
+        {
+          field: "sendMaster.open",
+          label: "功能开关",
+          bottomHelpMessage: "允许用户联系主人",
+          component: "Switch"
+        },
+        {
+          field: "sendMaster.cd",
+          label: "触发冷却",
+          bottomHelpMessage: "单位：秒，主人不受限制",
+          component: "Input",
+          required: true,
+          componentProps: {
+            placeholder: "请输入冷却时间"
+          }
+        },
+        {
+          field: "sendMaster.Master",
+          label: "主人配置",
+          helpMessage: "填主人QQ可发送某个指定主人",
+          bottomHelpMessage: "0：仅发送首个主人 1：发送全部主人 QQ号：发送指定QQ号",
+          component: "Input",
+          required: true,
+          componentProps: {
+            placeholder: "请输入主人QQ或配置项"
+          }
+        },
+        {
+          field: "sendMaster.Master",
+          label: "Bot配置",
+          bottomHelpMessage: "指定某个Bot发送，为0时为触发Bot",
+          component: "Input",
+          required: true,
+          componentProps: {
+            placeholder: "请输入Bot账号或配置项"
+          }
+        },
+        {
+          field: "sendMaster.sendAvatar",
+          label: "消息附带触发者头像",
+          bottomHelpMessage: "微信Bot请关闭此项，否则可能报错",
+          component: "Switch"
+        },
+        {
+          component: "Divider",
+          label: "戳一戳设置"
+        },
+        {
+          field: "other.chuo",
+          label: "戳一戳开关",
+          component: "Switch"
+        },
+        {
+          field: "other.chuoType",
+          label: "戳一戳图片类型",
+          bottomHelpMessage: "自定义图片自行放在resources/chuo/default中",
+          component: "RadioGroup",
+          required: true,
+          componentProps: {
+            options: [
+              { label: "柴郡表情包", value: 1 },
+              { label: "丛雨表情包", value: 2 },
+              { label: "诗歌剧表情包", value: 3 },
+              { label: "千恋万花表情包", value: 4 },
+              { label: "小南梁表情包", value: 5 },
+              { label: "自定义图片", value: 0 }
+            ]
+          }
+        },
+        {
+          component: "Divider",
+          label: "其他"
+        },
+        {
+          field: "other.ys",
+          label: "原神关键词发图",
+          helpMessage: "无用的功能+1",
+          component: "Switch"
+        }
+      ],
+      getConfigData() {
+        return {
+          other: Config.other,
+          sendMaster: Config.sendMaster
+        }
+      },
+      // 设置配置的方法（前端点确定后调用的方法）
+      setConfigData(data, { Result }) {
+        for (let key in data) {
+          Config.modify(...key.split("."), data[key])
+        }
+        return Result.ok({}, "Ciallo～(∠・ω< )⌒☆")
+      }
+    }
+  }
+}
