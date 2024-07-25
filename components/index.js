e06e2fc720efd4e8ed2e651187ca950c8d5813f8
kesally <admin@kesally.ren>
2024-07-25 09:09:59 +0000

    ✨ 添加DF帮助功能 * 细节优化 * 添加DF帮助

--- components/index.js
+++ components/index.js
@@ -1,9 +1,12 @@
 import Data from "./Data.js"
 import Config from "./Config.js"
 import YamlReader from "./YamlReader.js"
+import Version from "./Version.js"
+import Common from "./Common.js"
+import render from "./common-lib/render.js"
 
 const Path = process.cwd()
 const Plugin_Name = "DF-Plugin"
 const Plugin_Path = `${Path}/plugins/${Plugin_Name}`
 
-export { Config, Data, Path, Plugin_Name, Plugin_Path, YamlReader }
+export { Config, Data, Path, Plugin_Name, Plugin_Path, YamlReader, render ,Version ,Common }
